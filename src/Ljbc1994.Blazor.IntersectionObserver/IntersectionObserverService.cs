947a4854e9253c986b21d191b0d604f982ab03c1
Turk <epicturkey85@gmail.com>
2021-10-15 14:46:16 -0600

    fix: Failed to fetch dynamically imported module #35 (#36)
    * fix: Failed to fetch dynamically imported module
    #35
    
    * fix: #35 make scriptPath relative

--- src/Ljbc1994.Blazor.IntersectionObserver/IntersectionObserverService.cs
+++ src/Ljbc1994.Blazor.IntersectionObserver/IntersectionObserverService.cs
@@ -11,7 +11,7 @@ namespace Ljbc1994.Blazor.IntersectionObserver
 {
     public class IntersectionObserverService: IIntersectionObserverService, IAsyncDisposable
     {
-        private readonly string scriptPath = "/_content/BlazorIntersectionObserver/blazor-intersection-observer.min.js";
+        private readonly string scriptPath = "_content/BlazorIntersectionObserver/blazor-intersection-observer.min.js";
 
         private readonly Task<IJSObjectReference> moduleTask;
 
@@ -22,9 +22,10 @@ namespace Ljbc1994.Blazor.IntersectionObserver
         /// </summary>
         private readonly ConcurrentDictionary<string, IntersectionObserver> observers = new ConcurrentDictionary<string, IntersectionObserver>();
 
-        public IntersectionObserverService(IJSRuntime jsRuntime)
+        public IntersectionObserverService(IJSRuntime jsRuntime, NavigationManager navManager)
         {
-            this.moduleTask = jsRuntime.InvokeAsync<IJSObjectReference>("import", this.scriptPath).AsTask();
+            var path = navManager.ToAbsoluteUri(this.scriptPath);
+            this.moduleTask = jsRuntime.InvokeAsync<IJSObjectReference>("import", path).AsTask();
             this.objectRef = DotNetObjectReference.Create(this);
         }
 
